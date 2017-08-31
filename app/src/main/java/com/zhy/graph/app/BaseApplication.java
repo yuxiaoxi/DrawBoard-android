486d8c8d869de49d60b3541c9e2a9bb773146325
yuzhuo <yuzhuo@dianping.com>
2017-08-31 18:49:35 +0800

    修改画板

--- app/src/main/java/com/zhy/graph/app/BaseApplication.java
+++ app/src/main/java/com/zhy/graph/app/BaseApplication.java
@@ -7,7 +7,6 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-import com.squareup.leakcanary.LeakCanary;
 import com.umeng.analytics.MobclickAgent;
 import com.zhy.graph.R;
 import com.zhy.graph.network.HomeObserverHepler;
@@ -41,13 +40,6 @@ public class BaseApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
-		if (LeakCanary.isInAnalyzerProcess(this)) {
-			// This process is dedicated to LeakCanary for heap analysis.
-			// You should not init your app in this process.
-			return;
-		}
-		LeakCanary.install(this);
 
 		ShareSDK.initSDK(this);
 		Dhroid.init(this);
